dbcc2cc24b6a70e132704dc743890c92ddc82f50
lucas-hood <mostafa.bsh@gmail.com>
2022-05-24 08:25:36 +0430

    cleanned up extra codes which had been commented out

--- src/components/BudgetApp/ExpenseItem.js
+++ src/components/BudgetApp/ExpenseItem.js
@@ -29,31 +29,3 @@ const ExpenseItem = ({ expense }) => {
 }
 
 export default ExpenseItem
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-// const ExpenseItem = (props) => {
-//   return (
-//     <li key={props.id}>
-      
-//         <span>{props.id}</span>
-      
-//         {props.name}
-      
-//         {props.cost} $
-      
-      
-//         <button onClick={() => props.handleDelete(props.id)}>Delete Expense</button>
-      
-//     </li>
-//   )
-// }
-
-// export default ExpenseItem
--- src/components/BudgetApp/ExpenseList.js
+++ src/components/BudgetApp/ExpenseList.js
@@ -8,9 +8,6 @@ const ExpenseList = () => {
 
   const totalExpense = expenses.reduce((total, expense) => { return total + parseInt(expense.cost)}, 0)
 
-  // let totalExpense = 0
-  // expenses.forEach(expense => totalExpense += parseInt(expense.cost))
-
   return (
 
     <div className='expense-list-area'>
@@ -30,53 +27,3 @@ const ExpenseList = () => {
 }
 
 export default ExpenseList
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react'
-// import ExpenseItem from '../ExpenseItem/ExpenseItem';
-
-// const ExpenseList = () => {
-
-//   const [expenses, setExpenses] = useState([
-//     { id: 12, name: 'shopping', cost: 40 },
-//     { id: 13, name: 'holiday', cost: 400 },
-//     { id: 14, name: 'car service', cost: 50 },
-// 	]);
-
-//   const handleDelete = id => {
-//     const newExpenses = expenses.filter(expense => expense.id !== id)
-//     setExpenses(newExpenses)
-//   }
-
-//   return (
-//     <div>
-//       <h1>ExpenseList</h1>
-//       <ul>
-//         {expenses.map(expense => (
-//           <ExpenseItem
-//             // expenses={expenses}  
-          
-//             id={expense.id}
-//             name={expense.name}
-//             cost={expense.cost}
-//             handleDelete={handleDelete}
-//           />  
-//         ))}
-//       </ul>
-
-//     </div>
-//   )
-// }
-
-// export default ExpenseList
